20dfa09e98ca109ff91f035aeb00f08bef43bc1c
nikeshsanjay <153799284+nikeshsanjay@users.noreply.github.com>
2024-08-06 12:37:04 +0530

    Add files via upload

---
+++ day 9/backend2/iigvrh/src/main/java/com/garage/iigvrh/IigvrhApplication.java
@@ -0,0 +1,13 @@
+package com.garage.iigvrh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IigvrhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IigvrhApplication.class, args);
+	}
+
+}
